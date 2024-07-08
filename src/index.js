7c423cc79dc7adb02112eff0245fc072235ef6e5
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-07-08 23:04:43 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -76,12 +76,22 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
-      {numPizzas > 0 && (
+      {/* {numPizzas > 0 && (
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      )} */}
+
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later 😊</p>
       )}
 
       {/* <Pizza
@@ -116,7 +126,7 @@ function Pizza(props) {
 const Footer = () => {
   const hour = new Date().getHours();
   const openHour = 12;
-  const closeHour = 22;
+  const closeHour = 24;
   const isOPen = hour >= openHour && hour <= closeHour;
   console.log(isOPen);
   //   if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
@@ -124,11 +134,15 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      {isOPen && (
+      {isOPen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
