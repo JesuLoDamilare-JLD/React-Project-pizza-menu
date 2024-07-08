aa04836643dcb82fea4562a2f4fc27225e77a6bd
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-07-08 22:44:47 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -68,15 +68,21 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
 
       {/* <Pizza
         name="Pizza Spinaci"
@@ -118,7 +124,12 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleDateString()}. We 're currently open
+      {isOPen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 };
